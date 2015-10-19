5fa2dac7951a660c75a444534b54363388cee17a
choijaewoong <jwchoi0129@gmail.com>
2015-10-19 10:14:22 +0900

    Default SlidingMenu생성

--- app/src/main/java/com/example/androidchoi/jobdam/MainActivity.java
+++ app/src/main/java/com/example/androidchoi/jobdam/MainActivity.java
@@ -2,21 +2,49 @@ package com.example.androidchoi.jobdam;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+public class MainActivity extends SlidingFragmentActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG_MY_JOB = "myJob";
+    private static final String TAG_CARD_BOX = "cardBox";
+    private static final String TAG_JOB_INFO = "jobInfo";
+    private static final String TAG_BOARD_ = "board";
+    private static final String TAG_ALARM  = "alarm";
+    private static final String TAG_SETTING  = "setting";
+
+    SlidingMenu mSlidingMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setBehindContentView(R.layout.slidingmenu_main); //Setting SlidingMenu
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null){
+//            getSupportFragmentManager().beginTransaction().add(R.id.menu_container, new MenuFragment()).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new MyJobFragment()).commit();
+        }
+        mSlidingMenu = getSlidingMenu();
+        mSlidingMenu.setBehindWidthRes(R.dimen.menu_width);
+        mSlidingMenu.setShadowDrawable(R.drawable.shadow);
+        mSlidingMenu.setShadowWidthRes(R.dimen.shadow_width);
+        mSlidingMenu.setFadeDegree(0.0f);
+        mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_drawer);
+//        setSlidingActionBarEnabled(false);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -41,12 +69,19 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == android.R.id.home) {
+            toggle();
             return true;
         }
-
+        //noinspection SimplifiableIfStatement
+        else if (id == R.id.action_settings) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        return false;
     }
 }
