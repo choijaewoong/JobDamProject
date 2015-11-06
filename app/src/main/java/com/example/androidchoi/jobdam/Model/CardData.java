7e323a5c05809e9fdc55b7c05bb33986cd0c1d7d
choijaewoong <jwchoi0129@gmail.com>
2015-11-06 18:26:50 +0900

    Card Item Layout 수정 및 배경 색 지정  * 모든 화면의 배경색 지정 (아직 모두 적용 x)  * Card Item Layout 수정

--- app/src/main/java/com/example/androidchoi/jobdam/Model/CardData.java
+++ app/src/main/java/com/example/androidchoi/jobdam/Model/CardData.java
@@ -26,7 +26,10 @@ public class CardData implements Serializable {
     }
 
     public CardData(){
-        mTitle = "카드 제목";
-        mContent = "카드 내용";
+        mTitle = "Android Studio";
+        mContent = "1. 코드 최적화는 가장 나중에\n" +
+                "2. 수레바퀴를 다시 만들지 말라.\n" +
+                "  (만들어져 있는건 굳이 만들지 말자)\n" +
+                "   (gemFile,  JQuery)";
     }
 }
