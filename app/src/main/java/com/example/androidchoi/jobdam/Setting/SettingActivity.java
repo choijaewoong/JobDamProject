25a8ed624ea78988023448dbd9c54fd5830a2d15
choijaewoong <jwchoi0129@gmail.com>
2015-11-29 02:58:34 +0900

    설정 화면 구성 변경

--- app/src/main/java/com/example/androidchoi/jobdam/Setting/SettingActivity.java
+++ app/src/main/java/com/example/androidchoi/jobdam/Setting/SettingActivity.java
@@ -6,20 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-import com.example.androidchoi.jobdam.MainActivity;
+import com.example.androidchoi.jobdam.Model.User;
 import com.example.androidchoi.jobdam.R;
 
 public class SettingActivity extends AppCompatActivity {
-    public final static String[] SETTINGMENU
-            = {"Logout", "Help Desk", "Privacy", "Legal", "Alarm", "Version" };
 
-    ListView mListView;
-    ArrayAdapter<String> mAdapter;
+    TextView mTextUserEmail;
+    ToggleButton mAutoLogin;
+    ImageView mImageAlarmSetting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,48 +29,18 @@ public class SettingActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_back);
 
-        mListView = (ListView)findViewById(R.id.listview_setting);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mTextUserEmail = (TextView)findViewById(R.id.text_setting_user_email);
+        mTextUserEmail.setText(User.getInstance().getUserId());
+        mAutoLogin = (ToggleButton)findViewById(R.id.toggleButton_auto_login);
+        mImageAlarmSetting = (ImageView)findViewById(R.id.image_setting_alarm_button);
+        mImageAlarmSetting.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent;
-//                Toast.makeText(SettingActivity.this, mAdapter.getItem(position) + "/" + mAdapter.getItemId(position), Toast.LENGTH_SHORT).show();
-                switch ((int) mAdapter.getItemId(position)) {
-                    case 0:
-                        Toast.makeText(SettingActivity.this, "로그아웃!", Toast.LENGTH_SHORT).show();
-                        intent = new Intent(SettingActivity.this, MainActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-                        finish();
-                        break;
-                    case 1:
-                        intent = new Intent(SettingActivity.this, HelpDeskActivity.class);
-                        startActivity(intent);
-                        break;
-                    case 2:
-                        intent = new Intent(SettingActivity.this, PrivacyActivity.class);
-                        startActivity(intent);
-                        break;
-                    case 3:
-                        intent = new Intent(SettingActivity.this, LegalActivity.class);
-                        startActivity(intent);
-                        break;
-                    case 4:
-                        intent = new Intent(SettingActivity.this, AlarmSettingActivity.class);
-                        startActivity(intent);
-                        break;
-                    case 5:
-                        intent = new Intent(SettingActivity.this, VersionInfoActivity.class);
-                        startActivity(intent);
-                        break;
-                    default:
-                        break;
-                }
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, AlarmSettingActivity.class);
+                startActivity(intent);
             }
         });
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        mListView.setAdapter(mAdapter);
-        initSettingMenu();
+
     }
 
     @Override
@@ -87,7 +56,5 @@ public class SettingActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    private void initSettingMenu() {
-        mAdapter.addAll(SETTINGMENU);
-    }
+
 }
