f8d93eb4a7e1a426e63e5e184502273c63bf5ace
choijaewoong <jwchoi0129@gmail.com>
2015-11-29 13:00:51 +0900

    약관동의, 회원가입 화면 구성 및 기능 추가

--- app/src/main/java/com/example/androidchoi/jobdam/LogIn/ServiceAgreementFragment.java
+++ app/src/main/java/com/example/androidchoi/jobdam/LogIn/ServiceAgreementFragment.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.androidchoi.jobdam.R;
 
@@ -19,7 +23,10 @@ import com.example.androidchoi.jobdam.R;
  */
 public class ServiceAgreementFragment extends Fragment {
 
-
+    TextView mTextUserAgreement;
+    TextView mTextPrivateInfo;
+    CheckBox mCheckBox;
+    CheckBox mCheckBox2;
     public ServiceAgreementFragment() {
         // Required empty public constructor
     }
@@ -40,11 +47,23 @@ public class ServiceAgreementFragment extends Fragment {
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_back);
+
+        mTextUserAgreement = (TextView)view.findViewById(R.id.text_user_agreement);
+        mTextPrivateInfo = (TextView)view.findViewById(R.id.text_private_info);
+        mTextUserAgreement.setText(Html.fromHtml(getString(R.string.text_user_agreement)));
+        mTextPrivateInfo.setText(Html.fromHtml(getString(R.string.text_private_info)));
+
+        mCheckBox = (CheckBox)view.findViewById(R.id.checkBox_user_agreement);
+        mCheckBox2 = (CheckBox)view.findViewById(R.id.checkBox_private_info);
+
         Button button = (Button)view.findViewById(R.id.btn_next_level);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(mCheckBox.isChecked() && mCheckBox.isChecked())
                 getActivity().getSupportFragmentManager().beginTransaction().add(R.id.login_container, new InputUserInfoFragment()).addToBackStack(null).commit();
+                else
+                    Toast.makeText(getActivity(), "약관에 모두 동의해주세요.", Toast.LENGTH_SHORT).show();
             }
         });
 
--- app/src/main/java/com/example/androidchoi/jobdam/Setting/AlarmSettingActivity.java
+++ app/src/main/java/com/example/androidchoi/jobdam/Setting/AlarmSettingActivity.java
@@ -2,6 +2,8 @@ package com.example.androidchoi.jobdam.Setting;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.androidchoi.jobdam.R;
 
@@ -11,5 +13,25 @@ public class AlarmSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_setting);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.icon_back);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        //noinspection SimplifiableIfStatement
+        else if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
