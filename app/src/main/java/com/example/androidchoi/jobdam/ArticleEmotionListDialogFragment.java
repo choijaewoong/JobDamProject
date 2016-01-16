7ccda22a2cf4ae597d035f8b62ea6c6ca4d50a6a
choijaewoong <jwchoi0129@gmail.com>
2016-01-16 18:34:47 +0900

    잡담 톡 감정 데이터 생성 및 감정 다이얼로그 프래그먼트 생성

---
+++ app/src/main/java/com/example/androidchoi/jobdam/ArticleEmotionListDialogFragment.java
@@ -0,0 +1,31 @@
+package com.example.androidchoi.jobdam;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ArticleEmotionListDialogFragment extends DialogFragment {
+
+
+    public ArticleEmotionListDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_article_emotion_list_dialog, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/androidchoi/jobdam/Model/EmotionData.java
@@ -0,0 +1,51 @@
+package com.example.androidchoi.jobdam.Model;
+
+import android.content.Context;
+
+import com.example.androidchoi.jobdam.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tacademy on 2015-10-30.
+ */
+public class EmotionData {
+    private static EmotionData sEmotionData;
+    private Context mContext;
+
+    private ArrayList<EmotionData> mEmotionList;
+
+    public ArrayList<EmotionData> getCategoryList() {
+        return mEmotionList;
+    }
+
+    private EmotionData(Context context) {
+        mContext = context;
+        mEmotionList = new ArrayList<EmotionData>();
+        mEmotionList.add(new EmotionData(R.drawable.emotion_smile, "좋아요"));
+        mEmotionList.add(new EmotionData(R.drawable.emotion_happy, "행복해요"));
+        mEmotionList.add(new EmotionData(R.drawable.emotion_suprized, "놀라워요"));
+        mEmotionList.add(new EmotionData(R.drawable.emotion_complicated, "복잡미묘"));
+        mEmotionList.add(new EmotionData(R.drawable.emotion_puzzled, "어리둥절"));
+        mEmotionList.add(new EmotionData(R.drawable.emotion_depressed, "우울해요"));
+        mEmotionList.add(new EmotionData(R.drawable.emotion_sad, "속상해요"));
+    }
+
+    private EmotionData(int imageResource, String description) {
+        this.imageResource = imageResource;
+        this.description = description;
+    }
+
+    public static EmotionData get(Context context) {
+        if (sEmotionData == null) {
+            sEmotionData = new EmotionData(context.getApplicationContext());
+        }
+        return sEmotionData;
+    }
+    private int imageResource;
+    private String description;
+    public int getImage() {
+        return imageResource;
+    }
+    public String getDescription() { return description;}
+}
